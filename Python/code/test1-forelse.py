9c9ded8557db5aee35d551dcdefdb8dfa9ce2f9d
davidzheng66 <davidzheng66@gmail.com>
2020-08-03 16:42:01 -0400

    Add for else

---
+++ Python/code/test1-forelse.py
@@ -0,0 +1,12 @@
+nums = [11,12,13,16,21, 20]
+for i in nums:
+    if 0 == i%5:
+        print(i)
+        break
+else:
+    print('Not found')
+
+print("hello1", end=" ")
+print("hello2", end=" ")
+print()
+print("hello3", end=" ")

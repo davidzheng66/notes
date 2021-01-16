b81759dc3fb80c6514b5715e05ea72bfd8d840f9
David Zheng <davidzheng66@gmail.com>
2021-01-16 11:22:14 -0500

    add source version

--- linux/cmaketutorial/main.cpp
+++ linux/cmaketutorial/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <adder.hpp>
+#include <cmaketutorial_config.h>
+
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
+	cout << argv[0] << " Version " << CMAKETUTORIAL_VERSION_MAJOR << "." << CMAKETUTORIAL_VERSION_MINOR << endl;
 	cout << "Hello World!!!" << endl;
 	cout << add(10.0, 20.0) << endl;
 }	
